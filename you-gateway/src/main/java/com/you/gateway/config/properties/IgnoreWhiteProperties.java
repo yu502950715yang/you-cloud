f90a6b91881fa99b1fbd44d44ccab366edf101e5
余洋 <502950715@qq.com>
2023-08-09 17:12:30 +0800

    [修改] 代码重构

--- you-gateway/src/main/java/com/you/gateway/config/properties/IgnoreWhiteProperties.java
+++ you-gateway/src/main/java/com/you/gateway/config/properties/IgnoreWhiteProperties.java
@@ -1,5 +1,6 @@
 package com.you.gateway.config.properties;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import java.util.List;
  * @since 1.0
  * Create with Intellij IDEA on 2023-04-06 14:01
  */
+@Getter
 @Configuration
 @RefreshScope
 @ConfigurationProperties(prefix = "auth.ignore")
@@ -25,10 +27,6 @@ public class IgnoreWhiteProperties {
      * 放行url
      */
     private List<String> whiteList = new ArrayList<>();
-
-    public List<String> getWhiteList() {
-        return whiteList;
-    }
 
     public void setWhiteList(List<String> whiteList) {
         this.whiteList = whiteList;
