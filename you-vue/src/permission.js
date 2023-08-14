7741227a300d662a8bbca7ae6d867b271ee2b720
余洋 <502950715@qq.com>
2023-08-14 13:05:51 +0800

    【修改】路由权限修改

--- you-vue/src/permission.js
+++ you-vue/src/permission.js
@@ -22,26 +22,32 @@ router.beforeEach((to, from, next) => {
       next({ path: '/' })
       NProgress.done()
     } else {
-      isRelogin.show = true
-      // 判断当前用户是否已拉取完user_info信息
-      useUserStore().getInfo().then(() => {
-        isRelogin.show = false
-        usePermissionStore().generateRoutes().then(accessRoutes => {
-          // 根据roles权限生成可访问的路由表
-          accessRoutes.forEach(route => {
-            if (!isHttp(route.path)) {
-              router.addRoute(route) // 动态添加可访问路由表
-            }
+      if (useUserStore().permissions.length === 0) {
+        isRelogin.show = true
+        // 判断当前用户是否已拉取完user_info信息
+        useUserStore().getInfo().then(() => {
+          isRelogin.show = false
+          usePermissionStore().generateRoutes().then(accessRoutes => {
+            // 根据权限生成可访问的路由表
+            accessRoutes.forEach(route => {
+              if (!isHttp(route.path)) {
+                router.addRoute(route) // 动态添加可访问路由表
+              }
+            })
+            next({...to, replace: true}) // hack方法 确保addRoutes已完成
+          }).catch(err => {
+            ElMessage.error(err)
+            next({path: '/'})
           })
-          next({...to, replace: true}) // hack方法 确保addRoutes已完成
+        }).catch(err => {
+          useUserStore().logOut().then(() => {
+            ElMessage.error(err)
+            next({path: '/'})
+          })
         })
-      }).catch(err => {
-        useUserStore().logOut().then(() => {
-          ElMessage.error(err)
-          next({path: '/'})
-        })
-      })
-      next()
+      } else {
+        next()
+      }
     }
   } else {
     // 没有token
