337fec476dd3a886dd0fd99f99c7b968a51b6b90
余洋 <502950715@qq.com>
2023-08-14 17:56:42 +0800

    【修改】页面切换动画效果修改

--- you-vue/src/layout/components/AppMain.vue
+++ you-vue/src/layout/components/AppMain.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="app-main">
     <router-view v-slot="{ Component, route }">
-      <transition name="fade-transform">
+      <transition name="fade-transform" mode="out-in">
         <keep-alive :include="tagsViewStore.cachedViews">
           <component v-if="!route.meta.link" :is="Component" :key="route.path"/>
         </keep-alive>
       </transition>
     </router-view>
-    <iframe-toggle />
+    <iframe-toggle/>
   </section>
 </template>
 
--- you-vue/src/layout/components/IframeToggle/index.vue
+++ you-vue/src/layout/components/IframeToggle/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition-group name="fade-transform">
+  <transition-group name="fade">
     <inner-link
       v-for="(item, index) in tagsViewStore.iframeViews"
       :key="item.path"
@@ -13,7 +13,28 @@
 <script setup>
 import InnerLink from "../InnerLink/index"
 import useTagsViewStore from '@/store/modules/tagsView'
+import {useRoute} from "vue-router";
 
 const route = useRoute();
 const tagsViewStore = useTagsViewStore()
 </script>
+
+<style lang="scss">
+.fade-move, /* 对移动中的元素应用的过渡 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* 确保将离开的元素从布局流中删除
+  以便能够正确地计算移动的动画。 */
+.fade-leave-active {
+  position: absolute;
+}
+</style>
