264491f221e7a632703d51cdebf5585aefc87203
余洋 <502950715@qq.com>
2023-10-06 11:52:16 +0800

    【修改】feign配置修改，使用统一的配置文件

--- api/api-system/src/main/java/com/you/system/api/feign/RemoteUserService.java
+++ api/api-system/src/main/java/com/you/system/api/feign/RemoteUserService.java
@@ -2,7 +2,6 @@ package com.you.system.api.feign;
 
 import com.you.common.core.constant.ServiceNameConstants;
 import com.you.common.core.model.R;
-import com.you.system.api.config.FeignConfig;
 import com.you.system.api.failback.RemoteUserFallbackFactory;
 import com.you.system.model.LoginUser;
 import com.you.system.model.SysUser;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Create with Intellij IDEA on 2023-04-12 17:11
  */
 @FeignClient(contextId = "remoteUserService", value = ServiceNameConstants.SYSTEM_SERVICE,
-        fallbackFactory = RemoteUserFallbackFactory.class, configuration = FeignConfig.class)
+        fallbackFactory = RemoteUserFallbackFactory.class)
 public interface RemoteUserService {
 
     @GetMapping(value = "/user/{username}")
