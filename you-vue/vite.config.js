5cfe37715e24aa8b092c540d4b0e7b0b834c9168
yuyang <502950715@qq.com>
2024-01-30 11:59:20 +0800

    Revert "添加eslintrc配置"
    This reverts commit 8a737a99970d59e1db9bac25eff2e9c277eec5b9.

--- you-vue/vite.config.js
+++ you-vue/vite.config.js
@@ -1,7 +1,6 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import createVitePlugins from './vite/plugins'
-import ViteEslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
@@ -12,7 +11,7 @@ export default defineConfig(({ mode, command }) => {
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
     base: VITE_APP_ENV === 'production' ? '/' : '/',
-    plugins: [createVitePlugins(env, command === 'build'), ViteEslintPlugin()],
+    plugins: createVitePlugins(env, command === 'build'),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
