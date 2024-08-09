ba0d7b8276155d16fb03d5a4cc372cf3d300f33f
yuyang <502950715@qq.com>
2024-08-09 13:15:50 +0800

    日志修改

--- you-modules/you-module-system/src/main/java/com/you/system/config/AsyncConfig.java
+++ you-modules/you-module-system/src/main/java/com/you/system/config/AsyncConfig.java
@@ -1,11 +1,13 @@
 package com.you.system.config;
 
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableAsync
@@ -15,12 +17,19 @@ public class AsyncConfig {
      * 消费日志线程池
      */
     @Bean("operLogThreadPool")
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5); // 核心线程数
-        executor.setMaxPoolSize(10); // 最大线程数
-        executor.setQueueCapacity(20); // 队列容量
-        executor.initialize();
-        return executor;
+    public Executor threadPoolExecutor() {
+        int corePoolSize = 5; // 核心线程数
+        int maxPoolSize = 10; // 最大线程数
+        long keepAliveTime = 60L; // 线程空闲时间
+        int queueCapacity = 25; // 队列容量
+
+        return new ThreadPoolExecutor(
+                corePoolSize,
+                maxPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(queueCapacity),
+                new ThreadPoolExecutor.CallerRunsPolicy() // 饱和策略
+        );
     }
 }
